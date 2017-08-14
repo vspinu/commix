ea99f37edb27adb377e93c8dfe605024c592ca21
Vitalie Spinu <spinuvit@gmail.com>
2017-08-15 01:06:10 +0200

    Add .dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+ ;;; Directory Local Variables for Emacs
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((nil
+  (require-final-newline . t)
+  ;; not tabs in code
+  (indent-tabs-mode)
+  ;; remove trailing whitespace
+  (eval . (add-hook 'before-save-hook 'delete-trailing-whitespace nil t))))
